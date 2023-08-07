bbf43cda88a95a008b84ade7f7b08fa6aefc88e4
DietherDeLuna <138032715+DietherDeLuna@users.noreply.github.com>
2023-08-07 18:14:09 +0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -125,7 +125,7 @@ const Navbar = ({toggleSidebar}) => {
         : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
           <div>
             <div className='flex w-full items-center justify-between'>
-              <Image src='/../public/images/db.png' width='87' height='35' alt=''/>
+              <Image src={ navLogo} width='87' height='35' alt=''/>
 
               <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                 <AiOutlineClose/>
